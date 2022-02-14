43a7b96161677dc1316b1c22e8dd7cc216e9d55f
Rebar Ahmad <rebar.ahmad@app-logik.de>
2022-02-14 21:08:41 +0100

    expose serviceaccountcredential

--- lib/firebase_admin.dart
+++ lib/firebase_admin.dart
@@ -1,6 +1,7 @@
 library firebase_admin;
 
 export 'src/admin.dart';
+export 'src/auth/credential.dart' show ServiceAccountCredential;
 export 'src/app.dart' hide AppInternalsExtension;
 export 'src/auth.dart';
 export 'src/utils/error.dart';
